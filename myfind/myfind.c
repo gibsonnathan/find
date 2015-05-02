d81fff5524ca284692db3bc7d8281df273d7a107
Nathan Gibson <gibson.nathan.2011@gmail.com>
2015-05-02 18:52:04 -0400

    think im donw

--- myfind/myfind.c
+++ myfind/myfind.c
@@ -1,10 +1,14 @@
-//
-//  myfind.c
-//  myfind
-//
-//  Created by Nathan Gibson on 5/1/15.
-//  Copyright (c) 2015 Nathan Gibson. All rights reserved.
-//
+/*
+ *
+ * Nathan Gibson
+ * myfind.c
+ *
+ * myfind emulates some of the functionality of the unix find command
+ *
+ * myfind requires a directory, and optionally takes a type (either file or 
+ * directory) and a depth.
+ *
+ */
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
@@ -12,6 +16,9 @@
 #include <fcntl.h>
 #include <dirent.h>
 
+/*
+ *  checks to see if the given string is a file
+ */
 int is_file(char* file){
     struct stat fd_stat;
     if (stat(file, &fd_stat)) {
@@ -24,6 +31,9 @@ int is_file(char* file){
     return 0;
 }
 
+/*
+ *  checks to see if the given string is a directory
+ */
 int is_dir(char* file){
     struct stat fd_stat;
     if (stat(file, &fd_stat)) {
@@ -36,13 +46,20 @@ int is_dir(char* file){
     return 0;
 }
 
+/*
+ *   find takes a directory, a depth > 0, and a filter (f or d)
+ *
+ *   the directory is opened and traversed, directories and files
+ *   are printed (if not filtered) and then the directories are
+ *   recursively visited, this continues until either the maxdepth
+ *   is met or there are no more directories to visit
+ */
 void find(char* directory, int maxdepth, char* type){
     
+    //if maxdepth has reached 0 then recursing
     if (maxdepth != -1 && maxdepth == 0) {
         return;
     }
-    
-    
     
     DIR *fd;
     struct dirent *de;
@@ -50,42 +67,47 @@ void find(char* directory, int maxdepth, char* type){
     if ((fd = opendir(directory)) == NULL) {
         fprintf(stderr, "Unable to open '%s'\n",
                 directory);
-        exit(3);
+        exit(1);
     }
-        
     
     while ((de = readdir(fd))) {
-        
+        //dont recurse on self or parent
         if (strcmp(de -> d_name, ".") == 0 || strcmp(de -> d_name, "..") == 0) {
             continue;
         }
-        
+        //build path from the passed in directory and the directory found in it
         char* path = (char*)malloc(sizeof(directory) + sizeof(de ->d_name) + 1);
         strcat(path, directory);
         strcat(path, "/");
         strcat(path, de -> d_name);
         
         if (is_file(path)) {
+            //only print if the type file was supplied
             if (strcmp(type, "f") == 0 || strcmp(type, "b") == 0) {
                 printf("%s\n", path);
             }
             
         }
         if(is_dir(path)){
+            //only print if the type directory was supplied
             if (strcmp(type, "d") == 0 || strcmp(type, "b") == 0) {
                 printf("%s\n", path);
             }
+            //recursively visit directories in this folder
             find(path, maxdepth == -1 ? -1 : maxdepth -1, type);
         }
-        
     }
     closedir(fd);
 }
 
 int main(int argc, char *argv[]){
     
-    char* directory;
+    char* directory = " ";
+    
+    //default value for depth if it isnt supplied
     int maxdepth = -1;
+    
+    //default value for type
     char* type = "b";
     
     if (argc > 1) {
@@ -100,6 +122,20 @@ int main(int argc, char *argv[]){
         }else if (strcmp("-maxdepth", argv[i]) == 0){
             maxdepth = atoi(argv[i + 1]);
         }
+    }
+    
+    if (maxdepth != -1 && maxdepth < 0) {
+        fprintf(stderr, "Incorrect depth\n");
+        exit(1);
+    }
+    
+    if (strcmp(directory, " ") == 0) {
+        fprintf(stderr, "Usage: directory --type = <d or f> --maxdepth = <#>\n");
+        exit(1);
+    }
+    
+    if (maxdepth == 0) {
+        printf("%s\n", directory);
     }
     
     find(directory, maxdepth, type);
